87e60a6974f6acbd60891a26c92b5bbfef301484
Richard Spiers <richard.spiers@gmail.com>
2013-01-11 12:49:28 +0200

    Upgraded to target latest framework

--- SIPLib/Properties/AssemblyInfo.cs
+++ SIPLib/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
+[assembly: ContractNamespace("", ClrNamespace = "MyServiceModel.DtoTypes")]
