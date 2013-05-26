3e5db5934fee47903cd3a074e44df8d34101c22f
Richard Spiers <richard.spiers@gmail.com>
2013-05-26 14:59:21 +0200

    Added comments for documentation to Events.cs

--- SIPLib/src/SIP/Events.cs
+++ SIPLib/src/SIP/Events.cs
@@ -1,4 +1,17 @@
-﻿#region
+﻿// ***********************************************************************
+// Assembly         : SIPLib
+// Author           : Richard Spiers
+// Created          : 06-06-2012
+//
+// Last Modified By : Richard Spiers
+// Last Modified On : 02-02-2013
+// ***********************************************************************
+// <copyright file="Events.cs">
+//     Copyright (c) Richard Spiers. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+#region
 
 using System;
 
@@ -6,20 +19,35 @@ using System;
 
 namespace SIPLib.SIP
 {
-    public class Events
-    {
-    }
 
+    /// <summary>
+    /// Class SipMessageEventArgs. This is a simple class used to pass SIP messages around triggered on various events.
+    /// </summary>
     public class SipMessageEventArgs : EventArgs
     {
+        /// <summary>
+        /// The SIP message
+        /// </summary>
         public Message Message;
+        /// <summary>
+        /// The UA
+        /// </summary>
         public UserAgent UA;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SIPLib.SIP.SipMessageEventArgs"/> class based on a SIP message.
+        /// </summary>
+        /// <param name="transferredMessage">The transferred message.</param>
         public SipMessageEventArgs(Message transferredMessage)
         {
             Message = transferredMessage;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SIPLib.SIP.SipMessageEventArgs"/> class based on SIP message and useragent.
+        /// </summary>
+        /// <param name="transferredMessage">The transferred message.</param>
+        /// <param name="ua">The ua.</param>
         public SipMessageEventArgs(Message transferredMessage, UserAgent ua)
         {
             Message = transferredMessage;
@@ -27,11 +55,25 @@ namespace SIPLib.SIP
         }
     }
 
+    /// <summary>
+    /// Class StackErrorEventArgs. Used to pass error messages from the SIP stack.
+    /// </summary>
     public class StackErrorEventArgs : EventArgs
     {
+        /// <summary>
+        /// The exception
+        /// </summary>
         public Exception Exception;
+        /// <summary>
+        /// A string representing the function the error occured in.
+        /// </summary>
         public string Function;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SIPLib.SIP.StackErrorEventArgs"/> class.
+        /// </summary>
+        /// <param name="inputFunction">A string representing the function name the error occured in.</param>
+        /// <param name="e">An exception to report.</param>
         public StackErrorEventArgs(string inputFunction, Exception e)
         {
             Function = inputFunction;
@@ -39,11 +81,25 @@ namespace SIPLib.SIP
         }
     }
 
+    /// <summary>
+    /// Class RegistrationChangedEventArgs. Event class used to pass information about SIP registration changes.
+    /// </summary>
     public class RegistrationChangedEventArgs : EventArgs
     {
+        /// <summary>
+        /// The SIP message
+        /// </summary>
         public Message Message;
+        /// <summary>
+        /// The current or changed registration state
+        /// </summary>
         public string State;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SIPLib.SIP.RegistrationChangedEventArgs"/> class.
+        /// </summary>
+        /// <param name="s">The registration state.</param>
+        /// <param name="m">The SIP message.</param>
         public RegistrationChangedEventArgs(string s, Message m)
         {
             State = s;
@@ -51,12 +107,30 @@ namespace SIPLib.SIP
         }
     }
 
+    /// <summary>
+    /// Class RawEventArgs. Event class used to pass raw received data to higher levels.
+    /// </summary>
     public class RawEventArgs : EventArgs
     {
+        /// <summary>
+        /// The transmitted data
+        /// </summary>
         public string Data;
+        /// <summary>
+        /// Was the data sent or received ?
+        /// </summary>
         public bool Sent;
+        /// <summary>
+        /// A string array representing the source of the data
+        /// </summary>
         public string[] Src;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SIPLib.SIP.RawEventArgs"/> class.
+        /// </summary>
+        /// <param name="d">Data to shuffle around</param>
+        /// <param name="s">The source of the data</param>
+        /// <param name="sent">if set to <c>true</c> then event represents sent data.</param>
         public RawEventArgs(string d, string[] s, bool sent)
         {
             Data = d;
